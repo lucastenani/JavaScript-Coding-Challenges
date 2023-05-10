0d118dd1d92c87993c07d8d6038012f308b558dd
lucastenani <lucas.tenani3@gmail.com>
2023-05-10 17:28:37 -0300

    Application that compares one number with another

---
+++ 1_FUNDAMENTALS/numbers_comparison /index.js
@@ -0,0 +1,15 @@
+function compareNumbers() {
+  let firstNumber = Number(document.getElementById("firstNumber").value);
+  let secondNumber = Number(document.getElementById("secondNumber").value);
+  let message = "";
+
+  if (firstNumber === secondNumber) {
+    message = `The number ${firstNumber} is equal to the number ${secondNumber}`;
+  } else if (firstNumber > secondNumber) {
+    message = `The number ${firstNumber} is greater than the number ${secondNumber}`;
+  } else if (firstNumber < secondNumber) {
+    message = `The number ${firstNumber} is less than the number ${secondNumber}`;
+  }
+
+  document.getElementById("result").textContent = message;
+}
