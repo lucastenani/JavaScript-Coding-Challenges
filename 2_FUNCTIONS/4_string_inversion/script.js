e0ca7e39c97a60c803b1f9003346b0892df92f3e
lucastenani <lucas.tenani3@gmail.com>
2023-05-22 15:58:52 -0300

    Function that reverses string passed as a parameter

---
+++ 2_FUNCTIONS/4_string_inversion/script.js
@@ -0,0 +1,7 @@
+function stringInversion(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(stringInversion("JavaScript"));
+console.log(stringInversion("How are you?"));
+console.log(stringInversion("Anonymous"));
