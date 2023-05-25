98defbccfc031049987e104bb52444dc54b1b370
lucastenani <lucas.tenani3@gmail.com>
2023-05-25 18:47:39 -0300

    Function that returns the array without the duplicates

--- 3_ARRAYS_AND_OBJECTS/1_duplicate_elements/script.js
+++ 3_ARRAYS_AND_OBJECTS/1_duplicate_elements/script.js
@@ -3,8 +3,12 @@ function duplicateElements(array) {
 }
 
 console.log(duplicateElements([1, 2, 3, 4, 4, 5, 6, 6]));
+// Expected output: [ 1, 2, 3, 4, 5, 6 ]
 console.log(duplicateElements([7, 8, 9, 10]));
+// Expected output: [ 7, 8, 9, 10 ]
 console.log(
   duplicateElements(["apple", "banana", "apple", "orange", "banana"])
 );
+// Expected output: [ 'apple', 'banana', 'orange' ]
 console.log(duplicateElements([1, "apple", 2, "banana", "apple", 3, "banana"]));
+// Expected output: [ 1, 'apple', 2, 'banana', 3 ]
