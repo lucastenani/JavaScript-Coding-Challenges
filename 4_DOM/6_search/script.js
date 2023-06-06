a8d1ff63f50a5ff15f99dbb1bd8a9db098ea74ac
lucastenani <lucas.tenani3@gmail.com>
2023-06-06 18:15:48 -0300

    inserting constant inside the event

--- 4_DOM/6_search/script.js
+++ 4_DOM/6_search/script.js
@@ -1,11 +1,14 @@
-const searchValue = document.getElementById("search-value");
 const searchButton = document.getElementById("search-button");
 const gamesList = document.querySelector("ul");
 const gamesListArray = Array.from(gamesList.children);
 
 searchButton.addEventListener("click", function () {
+  const searchValue = document
+    .querySelector("#search-value")
+    .value.toLowerCase();
+
   gamesListArray.forEach(function (e) {
-    !e.textContent.toLowerCase().includes(searchValue.value.toLowerCase())
+    !e.textContent.toLowerCase().includes(searchValue)
       ? gamesList.removeChild(e)
       : "";
   });
