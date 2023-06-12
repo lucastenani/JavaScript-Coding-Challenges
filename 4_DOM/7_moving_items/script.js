4e1234f5520ba322996ddca87aa441b4fea19d68
lucastenani <lucas.tenani3@gmail.com>
2023-06-12 17:40:01 -0300

    function that moves list items

---
+++ 4_DOM/7_moving_items/script.js
@@ -0,0 +1,28 @@
+const gamesList = document.querySelector("ul");
+let selectedItem = null;
+
+function moveItem(direction) {
+  if (!selectedItem) {
+    return alert("Please select a game");
+  }
+
+  const gamesListArray = Array.from(gamesList.children);
+  const index = gamesListArray.indexOf(selectedItem);
+
+  if (direction === "go_up" && index > 0) {
+    gamesList.insertBefore(selectedItem, gamesListArray[index - 1]);
+  } else if (direction === "go_down" && index < gamesListArray.length - 1) {
+    gamesList.insertBefore(selectedItem.nextElementSibling, selectedItem);
+  }
+}
+
+function selectItem(event) {
+  if (selectedItem) {
+    selectedItem.classList.remove("selected");
+  }
+
+  event.target.classList.add("selected");
+  selectedItem = event.target;
+}
+
+gamesList.addEventListener("click", selectItem);
